e3635c31ef13e8a8247bc7948bfa2b2cf3c1838d
tung1998 <tung16021220@gmail.com>
2020-02-21 14:31:11 +0700

    update

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -1,1 +1,9 @@
 import './body.html';
+
+
+Template.App_body.onCreated(function () {
+    $('body').addClass('kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header-mobile--fixed kt-subheader--enabled kt-subheader--fixed kt-subheader--solid kt-aside--enabled kt-aside--fixed kt-footer--fixed kt-page--loading')
+});
+Template.App_body.onRendered(function () {
+    KTLayout.init();
+});
