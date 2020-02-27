eaf4f13ea80d754f6d8ac3c00f2dc897abd0b083
linh1122 <llinh9054@gmail.com>
2020-02-27 15:25:38 +0700

    update tripMannager

--- imports/ui/components/admin/tripMannager/tripMannager.js
+++ imports/ui/components/admin/tripMannager/tripMannager.js
@@ -15,17 +15,19 @@ Template.tripMannager.onCreated(() => {
     accessToken = Cookies.get('accessToken')
 })
 
-Template.tripMannager.onRendered(()=>{
+Template.tripMannager.onRendered(() => {
     reloadTable();
     renderRouteSelect();
 })
 
 Template.tripMannager.events({
     'submit form': SubmitForm,
+    'click .modify-button': ClickModifyButton,
+    'click add-more': ClickAddMoreButton
 })
 
-function renderRouteSelect(){
-    $("#start-time").datetimepicker();
+function renderRouteSelect() {
+    $("#start-time").datetimepicker({});
     MeteorCall(_METHODS.route.GetAll, null, accessToken).then(result => {
         let options = result.data.map(route => {
             return `<option value="${route._id}">${route.name}</option>`
@@ -34,20 +36,48 @@ function renderRouteSelect(){
     }).catch(handleError)
 }
 
-function SubmitForm(event){
+function ClickModifyButton(event) {
+    let data = $(event.currentTarget).data("json")
+    $("#start-time").val(moment(data.startTime).format("YYYY/MM/DD HH:mm"));
+    // console.log(moment(data.startTime).format("MMMM DD YYYY, h:mm"))
+    $("#routeSelect").val(data.route._id);
+
+    $('#editTripMannagerModal').modal('show')
+    // console.log($("#routeSelect").val())
+    $('#editTripMannagerModal').attr("modify", data._id)
+}
+
+function ClickAddMoreButton(event) {
+    $('#editTripMannagerModal').attr("modify", "")
+}
+
+function SubmitForm(event) {
     event.preventDefault();
     // console.log(moment($("#start-time").val(), "YYYY/MM/DD HH:mm").valueOf());
     let data = {
         startTime: moment($("#start-time").val(), "YYYY/MM/DD HH:mm").valueOf(),
         routeID: $("#routeSelect").val()
     }
-    MeteorCall(_METHODS.trip.Create, data, accessToken).then(result => {
-        reloadTable();
-        $('#editTripMannagerModal').modal('hide')
-    }).catch(handleError)
+    let modify = $('#editTripMannagerModal').attr("modify");
+    if (modify == "") {
+        MeteorCall(_METHODS.trip.Create, data, accessToken).then(result => {
+            reloadTable();
+            $('#editTripMannagerModal').modal('hide')
+        }).catch(handleError)
+    } else {
+        console.log(modify)
+        console.log(data)
+        data._id = modify;
+        MeteorCall(_METHODS.trip.Update, data, accessToken).then(result => {
+            
+            reloadTable();
+            $('#editTripMannagerModal').modal('hide')
+        })
+    }
+
 }
 
-function reloadTable(){
+function reloadTable() {
     MeteorCall(_METHODS.trip.GetAll, null, accessToken).then(result => {
         console.log(result);
         let html = result.data.map(htmlRow);
@@ -55,7 +85,7 @@ function reloadTable(){
     })
 }
 
-function htmlRow(data){
+function htmlRow(data) {
     return `<div class="kt-widget kt-widget--user-profile-3" style="background: bisque;
             border-radius: 20px !important;
             padding: 11px; margin-bottom: 10px;">
@@ -82,21 +112,26 @@ function htmlRow(data){
 
                         <div class="kt-widget__subhead" style="font-size: 15px;">
                             <div class="row">
-                                <i class="flaticon2-placeholder"></i>&nbsp;
+                                <i class="flaticon2-map"></i>&nbsp;
                                 <label for="route-name" class="form-control-label">Cung đường:</label>&nbsp;
                                 <a href="#" id="route-name">${data.route.name}</a>
                             </div>
                             <div class="row">
-                                <i class="flaticon2-calendar-9"></i>&nbsp;
+                                <i class="flaticon2-calendar-1"></i>&nbsp;
                                 <label for="time-start" class="form-control-label">Thời gian khởi hành:</label>&nbsp;
                                 <a href="#" id="time-start">${moment(data.startTime).format("DD MMM YY, h:mm")}</a>
                             </div>
                             <div class="row">
                                 <div class="col-12" style="padding-left: 0px;">
-                                    <i class="flaticon2-calendar-9"></i>&nbsp;
+                                    <i class="flaticon2-phone"></i>&nbsp;
                                     <label for="phonenumber" class="form-control-label">Liên hệ:</label>&nbsp;
                                     <a href="#" id="phonenumber">${data.route.driver.user.phone}</a>
-                                    <a href="tripMannager/${data._id}"><button type="button" class="btn btn-success btn-sm fa-pull-right">Xem thông tin</button></a>
+                                    <div class="fa-pull-right">
+                                        <button type="button" class="btn btn-danger btn-sm modify-button" data-json=\'${JSON.stringify(data)}\'>Sửa</button>
+                                        <span></span>
+                                        <a href="tripMannager/${data._id}"><button type="button" class="btn btn-success btn-sm">Xem thông tin</button></a>
+                                    </div>
+                                    
                                 </div>    
                             </div>
                         </div>
