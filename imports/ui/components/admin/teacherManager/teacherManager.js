b721515c0dfeecd09e65f5cc5d2c3dc2f1473650
tiennm <tiennm@fimo.edu.vn>
2020-03-16 08:56:12 +0700

    fix conflic

--- imports/ui/components/admin/teacherManager/teacherManager.js
+++ imports/ui/components/admin/teacherManager/teacherManager.js
@@ -5,7 +5,6 @@ import {
 const Cookies = require("js-cookie");
 
 import {
-<<<<<<< HEAD
   MeteorCall,
   handleError,
   handleSuccess,
@@ -21,22 +20,6 @@ import {
 import {
   _METHODS,
   LIMIT_DOCUMENT_PAGE
-=======
-    MeteorCall,
-    handleError,
-    handleSuccess,
-    handleConfirm,
-    addRequiredInputLabel,
-    addPaging,
-    tablePaging,
-    getBase64,
-    makeID
-} from "../../../../functions";
-
-import {
-    _METHODS,
-    LIMIT_DOCUMENT_PAGE
->>>>>>> 4bf349f1eaefa2beaef4b9ad702809ce61bbb600
 } from "../../../../variableConst";
 
 
@@ -49,7 +32,6 @@ Template.teacherManager.onCreated(() => {
 });
 
 Template.teacherManager.onRendered(() => {
-<<<<<<< HEAD
   addRequiredInputLabel()
   addPaging(),
   getLimitDocPerPage()
@@ -57,14 +39,6 @@ Template.teacherManager.onRendered(() => {
   renderSchoolName();
   initSelect2()
   initDropzone(".add-more", ".modify-button")
-=======
-    addRequiredInputLabel()
-    addPaging(),
-        getLimitDocPerPage()
-    reloadTable(1);
-    renderSchoolName();
-    initSelect2()
->>>>>>> 4bf349f1eaefa2beaef4b9ad702809ce61bbb600
 });
 
 Template.teacherManager.helpers({});
@@ -97,7 +71,6 @@ function renderSchoolName() {
 }
 
 function ClickModifyButton(e) {
-<<<<<<< HEAD
   let teacherData = $(e.currentTarget).data("json");
   $("#editTeacherModal").attr("teacherID", teacherData._id);
   $(".modal-title").html("Chỉnh Sửa");
@@ -116,21 +89,6 @@ function ClickModifyButton(e) {
   $('div.dropzone-previews').find('div.dz-image-preview').remove()
   $('div.dz-preview').show()
   $('.dropzone-msg-title').html("Kéo ảnh hoặc click để chọn ảnh.")
-=======
-    let teacherData = $(e.currentTarget).data("json");
-    $("#editTeacherModal").attr("teacherID", teacherData._id);
-    $(".modal-title").html("Chỉnh Sửa");
-    $(".confirm-button").html("Sửa");
-    $("#editTeacherModal").modal("show");
-
-    $(' input[name="name"]').val(teacherData.name);
-    $(' input[name="phoneNumber"]').val(teacherData.phone);
-    $(' input[name="email"]').val(teacherData.email);
-    $(' #school-select').val(teacherData.school).trigger('change');
-    $(' input[name="class"]').val(teacherData.class);
-
-    $("#school-select").val(teacherData.schoolID).trigger('change')
->>>>>>> 4bf349f1eaefa2beaef4b9ad702809ce61bbb600
 }
 
 function ClickAddmoreButton(e) {
@@ -161,7 +119,6 @@ function ClickDeleteButton(event) {
 }
 
 async function SubmitForm(event) {
-<<<<<<< HEAD
   try {
     event.preventDefault();
     if (checkInput()) {
@@ -214,74 +171,15 @@ async function SubmitForm(event) {
               reloadTable(currentPage, getLimitDocPerPage())
             })
             .catch(handleError);
-=======
-    try {
-        event.preventDefault();
-        if (checkInput()) {
-            const target = event.target;
-            if (checkInput()) {
-                let data = {
-                    name: target.name.value,
-                    username: target.phoneNumber.value,
-                    // address: target.address.value,
-                    phone: target.phoneNumber.value,
-                    email: target.email.value,
-                    password: "12345678",
-                    schoolID: $("#school-select").val(),
-                    schoolName: $('#select2-school-select-container').attr('title'),
-                };
-
-                if ($('#teacher-image').val()) {
-                    let imageId = makeID("user")
-                    let BASE64 = await getBase64($('#teacher-image')[0].files[0])
-                    let importImage = await MeteorCall(_METHODS.image.Import, {
-                        imageId,
-                        BASE64: [BASE64]
-                    }, accessToken)
-                    if (importImage.error)
-                        handleError(result, "Không tải được ảnh lên server!")
-                    else data.image = imageId
-                }
-                let modify = $("#editTeacherModal").attr("teacherID");
-                if (modify == "") {
-                    await MeteorCall(_METHODS.teacher.Create, data, accessToken)
-                        .then(result => {
-                            // console.log(result);
-                            handleSuccess("Thêm", "Giáo viên").then(() => {
-                                $("#editTeacherModal").modal("hide");
-                            })
-                            reloadTable(1, getLimitDocPerPage())
-                        })
-                        .catch(handleError);
-                } else {
-                    data._id = modify;
-                    await MeteorCall(_METHODS.teacher.Update, data, accessToken)
-                        .then(result => {
-                            // console.log(result);
-                            handleSuccess("Cập nhật", "Giáo viên").then(() => {
-                                $("#editTeacherModal").modal("hide");
-                            })
-                            reloadTable(currentPage, getLimitDocPerPage())
-                        })
-                        .catch(handleError);
-                }
-            }
->>>>>>> 4bf349f1eaefa2beaef4b9ad702809ce61bbb600
         }
-    } catch (error) {
+    }
+   }
+   } catch (error) {
         handleError(error)
     }
-<<<<<<< HEAD
-  } catch (error) {
-    handleError(error)
-  }
-=======
->>>>>>> 4bf349f1eaefa2beaef4b9ad702809ce61bbb600
-
 }
 
 function checkInput() {
-<<<<<<< HEAD
   let name = $("input[name='name']").val();
   let phone = $('input[name="phoneNumber"]').val();
   let email = $('input[name="email"]').val();
@@ -297,23 +195,6 @@ function checkInput() {
   } else {
     return true;
   }
-=======
-    let name = $("input[name='name']").val();
-    let phone = $('input[name="phoneNumber"]').val();
-    let email = $('input[name="email"]').val();
-    let schoolID = $("#school-select").val();
-
-    if (!schoolID || !name || !phone || !email) {
-        Swal.fire({
-            icon: "error",
-            text: "Làm ơn điền đầy đủ thông tin",
-            timer: 3000
-        })
-        return false;
-    } else {
-        return true;
-    }
->>>>>>> 4bf349f1eaefa2beaef4b9ad702809ce61bbb600
 
 }
 
@@ -324,14 +205,10 @@ function clearForm() {
     $(' input[name="email"]').val("");
     $(' input[name="avatar"]').val("");
 
-<<<<<<< HEAD
   $('#school-select').val('').trigger('change')
 
   // remove ảnh
   $('div.dropzone-previews').find('div.dz-preview').find('div.dz-image').find('img').attr('src', '')
-=======
-    $('#school-select').val('').trigger('change')
->>>>>>> 4bf349f1eaefa2beaef4b9ad702809ce61bbb600
 }
 
 function initSelect2() {
@@ -343,7 +220,6 @@ function initSelect2() {
 
 
 function getLimitDocPerPage() {
-<<<<<<< HEAD
   return parseInt($("#limit-doc").val());
 }
 
@@ -374,70 +250,10 @@ function reloadTable(page = 1, limitDocPerPage = LIMIT_DOCUMENT_PAGE) {
     }
     createTable(table, result, limitDocPerPage)
   })
-=======
-    return parseInt($("#limit-doc").val());
-}
-
-function reloadTable(page = 1, limitDocPerPage = LIMIT_DOCUMENT_PAGE) {
-    let table = $('#table-body');
-    let emptyWrapper = $('#empty-data');
-    table.html('');
-    MeteorCall(_METHODS.teacher.GetByPage, { page: page, limit: limitDocPerPage }, accessToken).then(result => {
-        console.log(result)
-        tablePaging(".tablePaging", result.count, page, limitDocPerPage)
-        $("#paging-detail").html(`Hiển thị ${limitDocPerPage} bản ghi`)
-        if (result.count === 0) {
-            $('.tablePaging').addClass('d-none');
-            table.parent().addClass('d-none');
-            emptyWrapper.removeClass('d-none');
-        } else if (result.count > limitDocPerPage) {
-            $('.tablePaging').removeClass('d-none');
-            table.parent().removeClass('d-none');
-            emptyWrapper.addClass('d-none');
-            // update số bản ghi
-        } else {
-            $('.tablePaging').addClass('d-none');
-            table.parent().removeClass('d-none');
-            emptyWrapper.addClass('d-none');
-        }
-        createTable(table, result, limitDocPerPage)
-    })
->>>>>>> 4bf349f1eaefa2beaef4b9ad702809ce61bbb600
 
 }
 
 function renderTable(data, page = 1) {
-<<<<<<< HEAD
-  let table = $('#table-body');
-  let emptyWrapper = $('#empty-data');
-  table.html('');
-  tablePaging('.tablePaging', data.count, page);
-  if (carStops.count === 0) {
-    $('.tablePaging').addClass('d-none');
-    table.parent().addClass('d-none');
-    emptyWrapper.removeClass('d-none');
-  } else {
-    $('.tablePaging').addClass('d-none');
-    table.parent().removeClass('d-none');
-    emptyWrapper.addClass('d-none');
-  }
-
-  createTable(table, data);
-}
-
-function createTable(table, result, limitDocPerPage) {
-  result.data.forEach((key, index) => {
-    key.index = index + (result.page - 1) * limitDocPerPage;
-    const row = createRow(key);
-    table.append(row);
-  });
-}
-
-function createRow(data) {
-  const data_row = dataRow(data);
-  // _id is tripID
-  return `
-=======
     let table = $('#table-body');
     let emptyWrapper = $('#empty-data');
     table.html('');
@@ -467,7 +283,6 @@ function createRow(data) {
     const data_row = dataRow(data);
     // _id is tripID
     return `
->>>>>>> 4bf349f1eaefa2beaef4b9ad702809ce61bbb600
         <tr id="${data._id}">
           ${data_row}
         </tr>
@@ -475,19 +290,6 @@ function createRow(data) {
 }
 
 function dataRow(result) {
-<<<<<<< HEAD
-  let data = {
-    _id: result._id,
-    name: result.user.name,
-    username: result.user.username,
-    phone: result.user.phone,
-    email: result.user.email,
-    schoolID: result.schoolID,
-    schoolName: result.school.name
-  };
-  return `
-                <td scope="row"></td>
-=======
     let data = {
         _id: result._id,
         name: result.user.name,
@@ -499,7 +301,6 @@ function dataRow(result) {
     };
     return `
                 <td scope="row">${result.index}</td>
->>>>>>> 4bf349f1eaefa2beaef4b9ad702809ce61bbb600
                 <td>${data.name}</td>
                 <td>${data.username}</td>
                 <td>${data.phone}</td>
