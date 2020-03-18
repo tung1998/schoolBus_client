084c4e5468abfe3922eccea8eae491811e348b4b
tung1998 <tung16021220@gmail.com>
2020-03-18 13:44:10 +0700

    update cònig

--- imports/api/config.js
+++ imports/api/config.js
@@ -4,8 +4,8 @@ export {
 }
 
 
-const BASE = 'http://123.24.137.209:2999'
-const AUTH_PATH = 'http://123.24.137.209:3000'
+// const BASE = 'http://123.24.137.209:2999'
+// const AUTH_PATH = 'http://123.24.137.209:3000'
 
 // const BASE = 'http://192.168.100.69:3000'
 // const AUTH_PATH = 'http://192.168.100.69:2999'
@@ -16,6 +16,8 @@ const AUTH_PATH = 'http://123.24.137.209:3000'
 
 
 // const AUTH_PATH = 'http://localhost:3000';
+
+const AUTH_PATH = 'http://linh.247car.vn';
 
 // console.log(BASE);
 // mail hainv@fimo
