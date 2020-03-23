c7ace9b2719287b9b6d5eda6c594f422556c078f
tung1998 <tung16021220@gmail.com>
2020-03-23 17:55:09 +0700

    update topbar

--- imports/ui/components/shared/mobileHeader/mobileHeader.js
+++ imports/ui/components/shared/mobileHeader/mobileHeader.js
@@ -1,1 +1,31 @@
 import './mobileHeader.html';
+import {
+    FlowRouter
+} from 'meteor/kadira:flow-router';
+import {
+    _SESSION
+} from '../../../../variableConst';
+Template.mobileHeader.events({
+    'click #signOut': sightOutClick
+})
+
+Template.mobileHeader.helpers({
+    username() {
+        return Session.get(_SESSION.username)
+    },
+    avata() {
+        return Session.get(_SESSION.avata)
+    },
+    name() {
+        return Session.get(_SESSION.name)
+    },
+})
+
+function sightOutClick() {
+    Cookies.remove('accessToken');
+    Session.set(_SESSION.name, null)
+    Session.set(_SESSION.avata, null)
+    Session.set(_SESSION.username, null)
+    Session.set(_SESSION.userID, null)
+    FlowRouter.go('/login')
+}
