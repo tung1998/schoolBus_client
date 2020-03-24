a848a95fa41715808163d7afff65d9a1f9ba49d5
tung1998 <tung16021220@gmail.com>
2020-03-24 16:50:50 +0700

    update filter

--- imports/ui/components/teacher/listClass/listClass.js
+++ imports/ui/components/teacher/listClass/listClass.js
@@ -1,1 +1,35 @@
 import './listClass.html'
+const Cookies = require("js-cookie");
+
+import {
+    MeteorCall,
+    handleError
+} from "../../../../functions";
+
+import {
+    _METHODS, _URL_images
+} from "../../../../variableConst";
+
+let accessToken;
+
+Template.listClass.onCreated(() => {
+    accessToken = Cookies.get("accessToken");
+    Session.set('classesInfo',[])
+});
+
+Template.listClass.onRendered(() => {
+    MeteorCall(_METHODS.class.GetAll, null, accessToken).then(result => {
+        console.log(result)
+        Session.set('classesInfo',result.data)
+    }).catch(handleError)
+});
+
+Template.listClass.helpers({
+    classesInfo(){
+        return Session.get('classesInfo')
+    }
+});
+
+Template.listClass.events({
+
+});
