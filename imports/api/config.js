eabe4898c7fe6c4cd73c4e4a3d529e91987c33b3
tung1998 <tung16021220@gmail.com>
2020-05-05 08:31:07 +0700

    update url

--- imports/api/config.js
+++ imports/api/config.js
@@ -4,22 +4,15 @@ export {
 }
 
 
-// const AUTH_PATH = 'http://123.24.137.209:3000'
-
-const BASE = 'http://113.190.128.251:3000'
-    const AUTH_PATH = 'http://113.190.128.251:2999'
-// const AUTH_PATH = 'http://14.231.130.100:2999'
 
 
-// const AUTH_PATH = 'http://192.168.100.69:3000';
-// const BASE = 'http://192.168.100.69:2999';
+const AUTH_PATH = 'http://192.168.0.111:2999';
+const BASE = 'http://192.168.0.111:3000';
+// const AUTH_PATH = 'http://192.168.0.111:3000';
 
 
-// const AUTH_PATH = 'http://localhost:3999';
 
-// const AUTH_PATH = 'http://linh.247car.vn';
 
-// console.log(BASE);
 // mail hainv@fimo
 export const FCM_ = {
     server_key: 'AAAAzsXbi0A:APA91bH0NyrKR7hQ5AZug1Qv94GMp10xQr_OGTMq05VkPbAkW_2NqVsJyaazvyia26GG25QTNSPREzjIOXKoS4kyx0SwQdEUztYxmytuKfBpOMqql_QlBPhkJ9pbcbteJRaqi7jxWq5HXnP5ddm1IRrVT8ZymSUblw',
