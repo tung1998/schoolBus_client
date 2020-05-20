518dd8ce587297473fd189d51dfcd6c897e37870
tung1998 <you@example.com>
2020-05-20 10:41:52 +0700

    fix

--- imports/api/methods/parrentRequest.js
+++ imports/api/methods/parrentRequest.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Methods related to links
 
 import { Meteor } from 'meteor/meteor';
@@ -44,50 +43,4 @@ function updateParrentRequest(data, accessToken = '') {
 function deleteParrentRequest(data, accessToken = '') {
     let url = `${BASE_ParentRequest}/${data._id}`;
     return httpDefault(METHOD.del, url, { token: accessToken });
-=======
-// Methods related to links
-
-import { Meteor } from 'meteor/meteor';
-
-import {
-    BASE,
-    AUTH_PATH
-} from '../config'
-import { METHOD, httpDefault } from '../checkAPI'
-const BASE_ParentRequest = `${AUTH_PATH}/ParentRequest`
-
-if (Meteor.isServer) {
-    Meteor.methods({
-        'ParrentRequest.create': createParrentRequest,
-        'ParrentRequest.getAll': getParrentRequests,
-        'ParrentRequest.getById': getParrentRequestById,
-        'ParrentRequest.update': updateParrentRequest,
-        'ParrentRequest.delete': deleteParrentRequest,
-    });
-}
-
-function getParrentRequests(data, accessToken = '') {
-    let url = `${BASE_ParentRequest}?extra=${data.extra}`;
-    return httpDefault(METHOD.get, url, { token: accessToken });
-}
-
-function getParrentRequestById(data, accessToken = '') {
-    let url = `${BASE_ParentRequest}/${data._id}`;
-    return httpDefault(METHOD.get, url, { token: accessToken });
-}
-
-function createParrentRequest(data, accessToken = '') {
-    let url = `${BASE_ParentRequest}`;
-    return httpDefault(METHOD.post, url, { body: data, token: accessToken });
-}
-
-function updateParrentRequest(data, accessToken = '') {
-    let url = `${BASE_ParentRequest}/${data._id}/content`;
-    return httpDefault(METHOD.put, url, { token: accessToken });
-}
-
-function deleteParrentRequest(data, accessToken = '') {
-    let url = `${BASE_ParentRequest}/${data._id}`;
-    return httpDefault(METHOD.del, url, { token: accessToken });
->>>>>>> a19aa0636d498d2983a7990aa63f507bc4d3a6b5
 }
