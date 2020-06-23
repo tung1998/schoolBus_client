ee0f783b0fbc9b2cce68ae63d3585d67143ca23c
tung1998 <tung16021220@gmail.com>
2020-06-23 13:23:03 +0700

    update realtime

--- imports/ui/components/admin/tripManager/tripInfo/tripInfo.js
+++ imports/ui/components/admin/tripManager/tripInfo/tripInfo.js
@@ -277,20 +277,11 @@ function clickOpenScannerModal() {
                     showFlipCameraButton: true, // iOS and Android
                     showTorchButton: true, // iOS and Android
                     torchOn: true, // Android, launch with the torch switched on (if available)
-<<<<<<< HEAD
                     prompt : "", // Android
                     resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
                     formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
                     orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
                     disableAnimations : true, // iOS
-=======
-                    saveHistory: true, // Android, save scan history (default false)
-                    prompt: "Place a barcode inside the scan area", // Android
-                    resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-                    formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-                    // orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
-                    disableAnimations: true, // iOS
->>>>>>> tiennm
                     disableSuccessBeep: false // iOS and Android
                 }
             );
